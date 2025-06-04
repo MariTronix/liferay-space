4b754d548ab88becf2133865e71d6d5dd77815d3
Guilherme Lira <guilherme.liratsouza@gmail.com>
2025-06-04 18:41:42 -0300

    responsividade página espaços

--- liferay-space/src/SpacesPage.tsx
+++ liferay-space/src/SpacesPage.tsx
@@ -10,7 +10,6 @@ export const SpacesPage = () => {
         <>
             <Header/>
             <BodySpacesPage />
-            <Testimonials />
             <WhySection />
             <Footer/>
         </>
