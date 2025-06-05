5f4f8d4b6ceb19d838b026921d9ca69090543deb
Mariana Mendes <filhatronixmmlima@gmail.com>
2025-06-05 20:44:51 -0300

    Testa config mínima do Vite

--- liferay-space/vite.config.ts
+++ liferay-space/vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { fileURLToPath } from 'url'; // Importação necessária
 
-// Padrão correto para recriar __dirname em ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Versão MÍNIMA para teste
 export default defineConfig({
-  server: {
-    host: '::',
-    port: 8080,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
